4b648ea1b9e234276151668a50de517530459b5f
Adej0la <dejoladaniel@gmail.com>
2022-02-23 16:11:20 +0100

    initial commit

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,35 @@
+// SPDX-License-identifier: UNLICENSED;
+
+pragma solidity ^0.8.0; //the compiler version 
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol"; //import the console logging function
+
+contract MyEpicNFT is ERC721URIStorage {
+
+    // This helps keep track of tokenIds
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+// Pass the name of our NFTs token and its symbol
+    constructor() ERC721 ("SquareNFT", "SQUARE") {
+        console.log("This is my NFT contract. Whoa!");
+    }
+
+    // A function our user will hit to get their NFT.
+    function makeAnEpicNFT() public {
+        // Get the current tokenId, this starts at 0.
+        uint256 newItemId = _tokenIds.current();
+
+        // actually mint the NFT to the sender using msg.sender.
+        _safeMint(msg.sender, newItemId);
+
+        // set the NFTs data.
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/MVTL");
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+        // Increment the counter for when the next NFT is minted
+        _tokenIds.increment();
+    }
+}
